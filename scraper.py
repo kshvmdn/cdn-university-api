c94513ecd2d6ca5a13f59701491c010b3b538403
Kashav Madan <kshvmdn@gmail.com>
2016-01-09 01:14:46 -0500

    Completed scraping for `admission` tab.
    Fixed NavigableString errors, using for loop with break. Going to try
    optimizing with a while after.

--- scraper.py
+++ scraper.py
@@ -22,27 +22,28 @@ def parse(response):
     if soup.find('h1', class_='template-heading'):
         prog_title = soup.find('h1', class_='template-heading').string
         # prog_school = soup.find('h2', class_='template-subheading').string
-    prog_info['overview'], prog_info['admission'] = {}, {}
-    prog_info['overview']['title'] = prog_title
+    prog_info['program'] = prog_title
+
+    prog_info['overview'], prog_info['admission'], prog_info['requirements'] = \
+        {}, {}, {}
+
     if soup.find('div', class_='tabbed-section'):
         overview = soup.find('div', id='overview')
         for tr in overview.find('table').find_all('tr'):
             key = tr.find_all('td')[0].string.lower().replace(' ', '_')
             value = tr.find_all('td')[1].text.strip()
             prog_info['overview'][key] = value
+
         admission = soup.find('div', id='admission')
         for h3 in admission.find(class_='tabbed-subsection').find_all('h3'):
             key = h3.text.lower().replace(' ', '_')
             value = []
-            while h3.next_sibling is not None and\
-                    (h3.next_sibling.name is None or h3.next_sibling.name == 'p'):
-                h3 = h3.next_sibling
-                print(type(h3))
-
-                if h3.string is not None and h3.string.strip() is not '':
-                    value.append(h3.text)
-            print('---')
-            # prog_info['admission'][key] = value
+            for sib in h3.next_siblings:
+                if sib.string is None or sib.string.strip() is not '':
+                    value.append(sib.text)
+                if sib.next_sibling is None or sib.next_sibling.name == 'h3':
+                    break
+            prog_info['admission'][key] = value
     pprint.pprint(prog_info, indent=2)
 
 
