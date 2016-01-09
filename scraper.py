6f0288777999d615d4869b9a563d1b0183ff247d
Kashav Madan <kshvmdn@gmail.com>
2016-01-09 00:03:20 -0500

    Scraping for `overview` tab

--- scraper.py
+++ scraper.py
@@ -17,5 +17,18 @@ def request(prog_code):
 
 def parse(response):
     """Parse response for the given program content and return data as a dict"""
-    return response.text
+    soup = bs4.BeautifulSoup(response.text, 'html.parser')
+    prog_info = {}
+    if soup.find('h1', class_='template-heading'):
+        prog_title = soup.find('h1', class_='template-heading').string
+        # prog_school = soup.find('h2', class_='template-subheading').string
+    prog_info['overview'], prog_info['admission'] = {}, {}
+    prog_info['overview']['title'] = prog_title
+    if soup.find('div', class_='tabbed-section'):
+        overview = soup.find('div', id='overview')
+        for tr in overview.find('table').find_all('tr'):
+            key = tr.find_all('td')[0].string.lower().replace(' ', '_')
+            value = tr.find_all('td')[1].text.strip()
+            prog_info['overview'][key] = value
+    pprint.pprint(prog_info)
 
